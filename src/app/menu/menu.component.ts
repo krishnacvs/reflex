f3d151753ad6f470efa5a9124a661ef88eb47127
krishnacvs@yahoo.com <krishnacvs@yahoo.com>
2018-12-18 14:03:43 -0500

    menu component

---
+++ src/app/menu/menu.component.ts
@@ -0,0 +1,15 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Todo } from '../todo';
+
+@Component(
+  {
+    selector: 'app-menu',
+    templateUrl: './menu.component.html',
+    styleUrls: ['./menu.component.css']
+  }
+)
+
+export class MenuComponent {
+  constructor() {
+  }
+}
