3781f4f8bf211dff32ca0e82d5aa830d938c4ff1
krishnacvs@yahoo.com <krishnacvs@yahoo.com>
2018-12-18 12:10:00 -0500

    Todo module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
+import { TodoDataService } from './todo-data.service';
 
 @Component({
-  selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"]
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  providers: []
 })
 export class AppComponent {
-  title = "CodeSandbox";
+
+  constructor(private todoDataService: TodoDataService) {
+  }
+
+  onAddTodo(todo) {
+    this.todoDataService.addTodo(todo);
+  }
+
+  onToggleTodoComplete(todo) {
+    this.todoDataService.toggleTodoComplete(todo);
+  }
+
+  onRemoveTodo(todo) {
+    this.todoDataService.deleteTodoById(todo.id);
+  }
+
+  get todos() {
+    return this.todoDataService.getAllTodos();
+  }
+
 }
